9d8aef219b4abc729c55ecd1f0c4a3b883b11274
Geeklords69 <kabirdhruw@protonmail.com>
2022-04-24 22:46:49 +0530

    changed redirect url

--- js/login.js
+++ js/login.js
@@ -19,7 +19,7 @@ function validate() {
 	var username = document.getElementById("username").value
 	var password = document.getElementById("password").value
 	if (username == 'abc' && password == "xyz") {
-		window.location.href = "product.html";
+		window.location.href = "https://dreamsoffashion.netlify.app/homepage";
 
 
 		return false
